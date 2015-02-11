5746321468a6caaee70b31f22999583d9d5bdaab
8BitRick <rickalo801@gmail.com>
2015-02-11 04:17:10 -0800

    Adding placeholder touch carousel.

--- app/assets/javascripts/cerulean.js
+++ app/assets/javascripts/cerulean.js
@@ -2,5 +2,7 @@
 // Bootswatch
 //= require jquery
 //= require jquery_ujs
+//= require jquery.touchcarousel-1.2
 //= require cerulean/loader
 //= require cerulean/bootswatch
+//= require tweet_it
---
+++ app/assets/javascripts/tweet_it.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("#carousel-image-and-text").touchCarousel({
+        pagingNav: false,
+        snapToItems: false,
+        itemsPerMove: 4,
+        scrollToLast: false,
+        loopItems: false,
+        scrollbar: true
+    });
+});
